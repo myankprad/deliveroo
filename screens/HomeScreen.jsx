6bd7d5f4abce31db8173f4cb1b1fd1ec618b9fc9
[mayank pradhan] <mayankpradhan115@gmail.com>
2023-03-31 19:52:38 +0530

    commit

---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text className="text-red-500">HomeScreen</Text>
+      <Text className="text-2xl text-red-800" >jai shree Ram</Text>
+      <Text className="text-4xl text-red-600">Ram ram jai raja ram ram ram jai sita ram ram ram jai raja ram ram ram jai seeta ram </Text>
+    </View>
+  )
+}
+
+export default HomeScreen
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+   content: ["./screens.{js,jsx,ts,tsx}",
+         "./pages/**/*.{js,jsx,ts,tsx}",
+         "./components/**/*.{js,jsx,ts,tsx}",
+         "./screens/**/*.{js,jsx,ts,tsx}",
+
+        ],
+         
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
